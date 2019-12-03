4ebfe83fe64bbd9f1bcdf8287c42abafdd6c3da4
Michael Weedman <michael.weedman@gmail.com>
2019-12-03 14:35:06 -0700

    Cleaned up

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require ('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -8,14 +8,18 @@ const moviedex = require('./movies-data.json');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(validateBearerToken);
 app.use(helmet());
 app.use(cors());
-console.log(process.env.API_TOKEN);
+
+app.use(validateBearerToken);
+
+
 
 function validateBearerToken(req,res,next){
   const apiToken = process.env.API_TOKEN;
+
   const authToken = req.get('Authorization');
+  
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
